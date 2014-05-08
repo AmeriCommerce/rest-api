2a785935fdb0c751c28c551ed036f40ac8105c6d
Chris Allen <crallend@gmail.com>
2014-05-08 10:57:30 -0500

    Fix js file tabs

--- examples/node/oauth_desktop_flow/oauth_desktop_flow.js
+++ examples/node/oauth_desktop_flow/oauth_desktop_flow.js
@@ -1,117 +1,117 @@
 var async = require('async'),
-		https = require('https'),
-		crypto = require('crypto');
+    https = require('https'),
+    crypto = require('crypto');
 
 exports.getAccessToken = function(options, next) {
 
-	var result = {},
-			locals = {};
+  var result = {},
+      locals = {};
 
-	async.series([
+  async.series([
 
-		function(callback) {
-			var sig = createSignature(options.secret, options.username, options.key, options.appId, options.appScope, options.storeDomain);
-			var outgoingData = JSON.stringify({
-				'app_id': options.appId,
-				'scope': options.appScope,
-				'redirect_url': options.storeDomain,
-				'username': options.username,
-				'signature': sig
-			});
-			var requestOptions = {
-				host: options.storeDomain,
-				port: 443,
-				path: '/api/oauth',
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'Content-Length': Buffer.byteLength(outgoingData, 'utf8')
-				}
-			};
-			var request = https.request(requestOptions, function(response) {
-				var data = '';
-				response.on('data', function(chunk) {
-					data += chunk;
-				});
-				response.on('end', function() {
-					var d = JSON.parse(data.toString());
-					locals.ref = d.ref;
-					locals.code = d.code;
-					callback();
-				});
-			});
-			request.write(outgoingData);
-			request.end();
-		},
+    function(callback) {
+      var sig = createSignature(options.secret, options.username, options.key, options.appId, options.appScope, options.storeDomain);
+      var outgoingData = JSON.stringify({
+        'app_id': options.appId,
+        'scope': options.appScope,
+        'redirect_url': options.storeDomain,
+        'username': options.username,
+        'signature': sig
+      });
+      var requestOptions = {
+        host: options.storeDomain,
+        port: 443,
+        path: '/api/oauth',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(outgoingData, 'utf8')
+        }
+      };
+      var request = https.request(requestOptions, function(response) {
+        var data = '';
+        response.on('data', function(chunk) {
+          data += chunk;
+        });
+        response.on('end', function() {
+          var d = JSON.parse(data.toString());
+          locals.ref = d.ref;
+          locals.code = d.code;
+          callback();
+        });
+      });
+      request.write(outgoingData);
+      request.end();
+    },
 
-		function(callback) {
-			var sig = createSignature(options.secret, locals.code, options.appId, options.appScope, options.storeDomain);
-			var outgoingData = JSON.stringify({
-				'app_id': options.appId,
-				'ref': locals.ref,
-				'signature': sig
-			});
-			var requestOptions = {
-				host: options.storeDomain,
-				port: 443,
-				path: '/api/oauth/access_token',
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'Content-Length': Buffer.byteLength(outgoingData, 'utf8')
-				}
-			};
-			var request = https.request(requestOptions, function(response) {
-				var data = '';
-				response.on('data', function(chunk) {
-					data += chunk;
-				});
-				response.on('end', function() {
-					var d = JSON.parse(data);
-					result.accessToken = d.access_token;
-					result.refreshToken = d.refresh_token;
-					result.expires = d.expires;
-					callback();
-				});
-			});
-			request.write(outgoingData);
-			request.end();
-		}
-	], function(err) {
-		if(err) return next(err);
-		next(undefined, result);
-	});
+    function(callback) {
+      var sig = createSignature(options.secret, locals.code, options.appId, options.appScope, options.storeDomain);
+      var outgoingData = JSON.stringify({
+        'app_id': options.appId,
+        'ref': locals.ref,
+        'signature': sig
+      });
+      var requestOptions = {
+        host: options.storeDomain,
+        port: 443,
+        path: '/api/oauth/access_token',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(outgoingData, 'utf8')
+        }
+      };
+      var request = https.request(requestOptions, function(response) {
+        var data = '';
+        response.on('data', function(chunk) {
+          data += chunk;
+        });
+        response.on('end', function() {
+          var d = JSON.parse(data);
+          result.accessToken = d.access_token;
+          result.refreshToken = d.refresh_token;
+          result.expires = d.expires;
+          callback();
+        });
+      });
+      request.write(outgoingData);
+      request.end();
+    }
+  ], function(err) {
+    if(err) return next(err);
+    next(undefined, result);
+  });
 
 };
 
 exports.getProductList = function(options, next) {
-	var requestOptions = {
-		host: options.storeDomain,
-		port: 443,
-		path: '/api/v1/products',
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-AC-Auth-Token': options.accessToken
-		}
-	};
-	var request = https.request(requestOptions, function(response) {
-		var data = '';
-		response.on('data', function(chunk) {
-			data += chunk;
-		});
-		response.on('end', function() {
-			var d = JSON.parse(data);
-			next(d);
-		});
-	});
-	request.end();
+  var requestOptions = {
+    host: options.storeDomain,
+    port: 443,
+    path: '/api/v1/products',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-AC-Auth-Token': options.accessToken
+    }
+  };
+  var request = https.request(requestOptions, function(response) {
+    var data = '';
+    response.on('data', function(chunk) {
+      data += chunk;
+    });
+    response.on('end', function() {
+      var d = JSON.parse(data);
+      next(d);
+    });
+  });
+  request.end();
 };
 
 function createSignature() {
-	var args = Array.prototype.slice.call(arguments),
-			combined = args.join(""),
-			sha256 = crypto.createHash('sha256');
+  var args = Array.prototype.slice.call(arguments),
+      combined = args.join(""),
+      sha256 = crypto.createHash('sha256');
 
-	return sha256.update(combined, 'utf8').digest('hex');
+  return sha256.update(combined, 'utf8').digest('hex');
 }
--- examples/node/oauth_desktop_flow/oauth_desktop_flow_usage.js
+++ examples/node/oauth_desktop_flow/oauth_desktop_flow_usage.js
@@ -1,67 +1,67 @@
 var async = require('async'),
-		readline = require('readline'),
-		acapi = require('./oauth_desktop_flow');
+    readline = require('readline'),
+    acapi = require('./oauth_desktop_flow');
 
 var APP_ID = 1,
-		APP_SCOPE = 'catalog',
-		APP_SECRET = 'YOUR_SECRET_DO_NOT_EXPOSE_THIS',
-		STORE_DOMAIN = 'your_store_domain.com';
+    APP_SCOPE = 'catalog',
+    APP_SECRET = 'YOUR_SECRET_DO_NOT_EXPOSE_THIS',
+    STORE_DOMAIN = 'your_store_domain.com';
 
 
 var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 var locals = {},
-		productList;
+    productList;
 
 async.series([
 
-	function(callback) {
-		rl.question('Username: ', function(answer) {
-			locals.username = answer;
-			callback();
-		});
-	},
+  function(callback) {
+    rl.question('Username: ', function(answer) {
+      locals.username = answer;
+      callback();
+    });
+  },
 
-	function(callback) {
-		rl.question('User API Key: ', function(answer) {
-			locals.key = answer;
-			callback();
-		});
-	},
+  function(callback) {
+    rl.question('User API Key: ', function(answer) {
+      locals.key = answer;
+      callback();
+    });
+  },
 
-	function(callback) {
-		var options = {
-			username: locals.username,
-			key: locals.key,
-			secret: APP_SECRET,
-			appId: APP_ID,
-			appScope: APP_SCOPE,
-			storeDomain: STORE_DOMAIN
-		};
-		acapi.getAccessToken(options, function(err, result) {
-			locals.accessToken = result.accessToken;
-			callback();
-		});
-	},
+  function(callback) {
+    var options = {
+      username: locals.username,
+      key: locals.key,
+      secret: APP_SECRET,
+      appId: APP_ID,
+      appScope: APP_SCOPE,
+      storeDomain: STORE_DOMAIN
+    };
+    acapi.getAccessToken(options, function(err, result) {
+      locals.accessToken = result.accessToken;
+      callback();
+    });
+  },
 
-	function(callback) {
-		var options = {
-			storeDomain: STORE_DOMAIN,
-			accessToken: locals.accessToken
-		};
-		acapi.getProductList(options, function(result) {
-			productList = result;
-			callback();
-		});
-	}
+  function(callback) {
+    var options = {
+      storeDomain: STORE_DOMAIN,
+      accessToken: locals.accessToken
+    };
+    acapi.getProductList(options, function(result) {
+      productList = result;
+      callback();
+    });
+  }
 
 ], function(err) {
-	if(err) console.log(err);
-	rl.close();
-	for(var i = 0, len = productList.products.length; i < len; i++) {
-		console.log(productList.products[i].item_name + ": " + productList.products[i].price);
-	}
+  if(err) console.log(err);
+  rl.close();
+  for(var i = 0, len = productList.products.length; i < len; i++) {
+    console.log(productList.products[i].item_name + ": " + productList.products[i].price);
+  }
 });
