a682dd53ad7f9386de540079e5771183a384c308
Chris Allen <crallend@gmail.com>
2014-05-15 11:54:40 -0500

    Big cleanup of the node example

---
+++ examples/node/oauth_desktop_flow/config.js
@@ -0,0 +1,13 @@
+config = {
+
+  appId: 1,
+
+  appSecret: "<< YOUR SECRET DO NOT EXPOSE THIS >>",
+
+  storeDomain: "<< YOUR STORE DOMAIN >>",
+
+  appScope: "catalog"
+  
+};
+
+module.exports = config;
