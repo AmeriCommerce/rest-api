b6c19c3c89ddde829929fe91ae8896606addb958
Chris Allen <crallend@gmail.com>
2014-05-20 10:32:06 -0700

    Fix more tabs

--- examples/node/oauth_web_flow/routes.js
+++ examples/node/oauth_web_flow/routes.js
@@ -3,48 +3,48 @@ var router = express.Router();
 var api = require('./americommerce');
 
 router.get('/', function(req, res) {
-	if(!req.session.apiUser) {
-		var returnUrl = 'http://localhost:3000/callback?r=' + encodeURIComponent('http://localhost:3000');
-		var redirectUrl = api.startNegotiation(returnUrl);
+  if(!req.session.apiUser) {
+    var returnUrl = 'http://localhost:3000/callback?r=' + encodeURIComponent('http://localhost:3000');
+    var redirectUrl = api.startNegotiation(returnUrl);
 
-		res.redirect(redirectUrl);
-		return;
-	}
-	var token = req.session.apiUser;
-	var page = req.query.page ? parseInt(req.query.page) : 1;
+    res.redirect(redirectUrl);
+    return;
+  }
+  var token = req.session.apiUser;
+  var page = req.query.page ? parseInt(req.query.page) : 1;
 
-	api.getProductList(token.access_token, page, function(productList) {
-		var nextButtonClass = ['next'];
-		var previousButtonClass = ['previous'];
+  api.getProductList(token.access_token, page, function(productList) {
+    var nextButtonClass = ['next'];
+    var previousButtonClass = ['previous'];
 
-		if(!productList.next_page) {
-			nextButtonClass.push('disabled');
-		}
-		if(!productList.previous_page) {
-			previousButtonClass.push('disabled');
-		}
+    if(!productList.next_page) {
+      nextButtonClass.push('disabled');
+    }
+    if(!productList.previous_page) {
+      previousButtonClass.push('disabled');
+    }
 
-		res.render('index', {
-			productList: productList,
-			nextButtonClass: nextButtonClass,
-			previousButtonClass: previousButtonClass,
-			currentPage: page
-		});
-	});
+    res.render('index', {
+      productList: productList,
+      nextButtonClass: nextButtonClass,
+      previousButtonClass: previousButtonClass,
+      currentPage: page
+    });
+  });
 });
 
 router.get('/callback', function(req, res) {
-	var r = req.query.r;
-	var originalReturnUrl = 'http://localhost:3000/callback?r=' + encodeURIComponent(r);
-	var tempData = {
-		authId: req.query.auth_id,
-		code: req.query.code,
-		returnUrl: originalReturnUrl
-	};
-	api.getToken(tempData, function(tokenData) {
-		req.session.apiUser = tokenData;
-		res.redirect(r);
-	});
+  var r = req.query.r;
+  var originalReturnUrl = 'http://localhost:3000/callback?r=' + encodeURIComponent(r);
+  var tempData = {
+    authId: req.query.auth_id,
+    code: req.query.code,
+    returnUrl: originalReturnUrl
+  };
+  api.getToken(tempData, function(tokenData) {
+    req.session.apiUser = tokenData;
+    res.redirect(r);
+  });
 });
 
 module.exports = router;
