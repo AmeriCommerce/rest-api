84d8be9c8057d57de0bfc11c9579bab692ca9f11
Chris Allen <crallend@gmail.com>
2014-07-17 11:53:31 -0500

    Remove all desktop flow examples

--- examples/csharp/OAuthDesktopFlowExample/OAuthDesktopFlowExample/Models/TokenVerify.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OAuthDesktopFlowExample.Models
-{
-    public class TokenVerify
-    {
-        public int AppId { get; set; } 
-        public int AuthId { get; set; }
-        public string Signature { get; set; }
-    }
-}
