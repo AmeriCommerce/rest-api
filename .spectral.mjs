b87fe97b4e9766cc9345ae3dc517ff8275385623
agsmithis@gmail.com <agsmithis@gmail.com>
2023-09-21 16:02:10 -0500

    UpdateCustomers

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5MTIy/spectral.js?branch_slug=master&token=6059ff28-652b-46c5-a79e-17b0480eec55";
+export default { extends: ruleset };
